fbdf86a0b70fb09dcb60ae4b4f007197eef71973
Gojo Satoru <92911145+Cloder07@users.noreply.github.com>
2022-05-12 08:20:53 +0700

    Update sosmed.py

--- userbot/modules/sosmed.py
+++ userbot/modules/sosmed.py
@@ -1,6 +1,6 @@
 # Copyright (C) Pocongonlen
 # PocongUserbot < https://github.com/poocong/PocongUserbot >
-# Recode @Gojo_satoru44
+# Recode @Daijobu69
 # All rights reserved.
 # Licensed under the Raphielscape Public License, Version 1.d (the "License");
 # you may not use this file except in compliance with the License.
